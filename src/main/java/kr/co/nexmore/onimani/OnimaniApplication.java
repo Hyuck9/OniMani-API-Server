c9e25c2b4d71b98255d93f9ee068e0912383d0bd
Hyuck <lhg1304@nexmore.co.kr>
2021-03-15 16:29:21 +0900

    Initial Commit

---
+++ src/main/java/kr/co/nexmore/onimani/OnimaniApplication.java
@@ -0,0 +1,13 @@
+package kr.co.nexmore.onimani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnimaniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnimaniApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/nexmore/onimani/OnimaniApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.nexmore.onimani;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnimaniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
